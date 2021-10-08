11115c4e5c8ce432c166ccb0fcacdcedadd899d8
danijha-lee <84164688+danijha-lee@users.noreply.github.com>
2021-10-08 16:41:37 -0400

    done

--- js/site.js
+++ js/site.js
@@ -63,7 +63,7 @@ let events = [{
     }
 ]
 
-buildDropDown()
+
 
 //build a dropdown of distinct cities
 function buildDropDown() {
@@ -73,14 +73,15 @@ function buildDropDown() {
     //clear out the drop down
     eventDD.innerHTML = "";
     //Get the template
-    let ddTemplate = document.getElementById("cityDd-template");
+    let ddTemplate = document.getElementById("cityDD-template");
 
-    let curEvents = events;
+    let curEvents = JSON.parse(localStorage.getItem("eventData")) || events;
 
+    
     //get unique values from the array
     let distinctEvents = [...new Set(curEvents.map((event) => event.city))];
 
-    let ddItemNode = document.importNode(ddTemplate.contentEditable, true);
+    let ddItemNode = document.importNode(ddTemplate.content, true);
     
     let ddItem = ddItemNode.querySelector("a");
     //<li> <a class = "dropdown-item" onclick ="getEvents(this)"></a></li>
@@ -91,5 +92,131 @@ function buildDropDown() {
     ddItem.textContent = "All";
      //<li> <a class = "dropdown-item" data-string="All"  onclick ="getEvents(this)">All</a></li>
     
-    eventDD.appendChild(ddItem);
+    eventDD.appendChild(ddItemNode);
+
+    for (let index = 0; index < distinctEvents.length; index++) {
+       ddItemNode= document.importNode(ddTemplate.content, true);
+        ddItem = ddItemNode.querySelector("a");
+        ddItem.setAttribute("data-string", distinctEvents[index]);
+        ddItem.textContent = distinctEvents[index];
+        eventDD.appendChild(ddItemNode);
+        
+    }
+    //display stats for all events
+    displayStats(curEvents);
+    
+    displayData();
 }
+
+//displays data for the current events
+function displayData() {
+    let template = document.getElementById("eventData-template");
+    let eventBody = document.getElementById("eventBody");
+
+     eventBody.innerHTML = "";
+    let curEvents = JSON.parse(localStorage.getItem("eventData")) || events;
+
+    for (let index = 0; index < curEvents.length; index++) {
+        let eventRow = document.importNode(template.content, true);
+        let eventCols = eventRow.querySelectorAll("td");
+
+        eventCols[0].textContent = curEvents[index].event;
+        eventCols[1].textContent = curEvents[index].city;
+        eventCols[2].textContent = curEvents[index].state;
+        eventCols[3].textContent = curEvents[index].attendance;
+        eventCols[4].textContent = new Date(
+            curEvents[index].date).toLocaleString();
+        
+        eventBody.appendChild(eventRow);
+            
+    }
+}
+//display stats for the filtered events "array"
+function displayStats(filteredEvents) {
+    let total = 0;
+    let average = 0;
+    let most = 0;
+    let least = -1;
+    let currentAttendance = 0;
+    
+    for (let index = 0; index < filteredEvents.length; index++) {
+        currentAttendance = filteredEvents[index].attendance;
+        total += currentAttendance;
+
+        if (most < currentAttendance) {
+            most = currentAttendance;
+            
+        }if (least > currentAttendance || least < 0) {
+            least = currentAttendance;
+        }
+        
+    }
+    //calculate the events for the selected City
+    average = total / filteredEvents.length;
+
+    document.getElementById("total").innerHTML = total.toLocaleString();
+    document.getElementById("most").innerHTML = most.toLocaleString();
+    document.getElementById("average").innerHTML = average.toLocaleString(
+        undefined, {
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 0
+        }
+    )
+    document.getElementById("least").innerHTML = least.toLocaleString();
+}
+
+//get the events for the selected City
+function getEvents(ddElement) {
+    
+    let cityName = ddElement.getAttribute("data-string");
+   
+     let curEvents = JSON.parse(localStorage.getItem("eventData")) || events;
+    let filteredEvents = curEvents;
+    document.getElementById("statsHeader").innerHTML = `Stats for ${cityName} Events`
+
+    if (cityName != "All") {
+        filteredEvents = curEvents.filter(function (item) {
+           if (item.city == cityName) {
+               return item;
+           } 
+        })
+        
+    }
+    displayStats(filteredEvents);
+}
+
+//save event to local storage
+function saveData() {
+    let curEvents = JSON.parse(localStorage.getItem("eventData")) || events;
+    let stateSelect = document.getElementById("newEventState");
+    let eventDate = document.getElementById("addDate").value;
+    let eventDate2 = `${eventDate} 00:00`;
+    let newEvent =
+    {
+        event: document.getElementById("addEventName").value,
+        city: document.getElementById("addCity").value,
+        state: stateSelect.options[stateSelect.selectedIndex].text,
+        attendance: parseInt(document.getElementById("addAttendance").value, 10),
+        date: new Date(eventDate2).toLocaleDateString()
+
+    };
+    
+    //push this object we just created to the curEvents array
+    curEvents.push(newEvent);
+    //Save Changes to the DataBase (local Storage)
+    localStorage.setItem("eventData", JSON.stringify(curEvents));
+    
+    buildDropDown();
+    displayData();
+    
+    swal({
+        title: "Thank You!",
+        text: `Your Event ${newEvent.event}  has been added!`,
+        icon: "success",
+    });
+  
+}
+
+// function dismissModal(){
+// $('#exampleModal').modal('hide');
+// }
